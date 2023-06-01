28299b2f906a2c1c1aebd99034b4872dda32f481
sy <sy.smythe@gmail.com>
2023-06-01 12:08:34 -0400

    Add profile pic to app header

--- components/AppHeader/AppHeader.tsx
+++ components/AppHeader/AppHeader.tsx
@@ -1,23 +1,43 @@
-type Props = {}
-export default function AppHeader({}: Props) {
-  return (
+import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from "next/image";
 
-    <div className="flex justify-between items-center gap-4 text-white bg-slate-950 py-4 px-6
-      h-[var(--header-height)]">
+type Props = {};
+export default function AppHeader({}: Props) {
+  const { user } = useUser();
+  return (
+    <div
+      className="flex h-[var(--header-height)] items-center justify-between gap-4 bg-slate-950 px-6 py-4
+      text-white"
+    >
+      {/* LEFT-SIDE CONTROLS */}
       <div className="flex items-center gap-3">
-        <div className="w-6 h-6 bg-slate-800">
-          {/* menu icon goes here*/}
+        <div className="h-6 w-6 bg-slate-800">
+          {/* menu icon or app-logo goes here*/}
         </div>
-        <div className="text-xl text-white/80 font-medium hover:text-white">
+        <div className="text-xl font-medium text-white/80 hover:text-white">
           WrenChat
         </div>
-        <div className="text-xs text-blue-300 font-mono border rounded-md border-blue-300 px-2 py-0">
+        <div className="rounded-md border border-blue-300 px-2 py-0 font-mono text-xs text-blue-300">
           alpha-0
         </div>
       </div>
+
+      {/* RIGHT-SIDE CONTROLS */}
       <div className="flex gap-4">
-        <div className="h-8 aspect-square bg-gray-600 rounded-full"></div>
+        
+          <div>
+            { user && 
+              <Image
+                src={String(user?.picture)}
+                width={30}
+                height={30}
+                alt="User avatar"
+                className="rounded-full"
+              />
+            }
+          
+        </div>
       </div>
     </div>
-  )
+  );
 }
